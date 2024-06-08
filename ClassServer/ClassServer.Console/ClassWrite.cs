76abd959a004941ba63e477e905c44b746ace8f8
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-06-08 15:15:25 +0800

    ClassServer

--- ClassServer/ClassServer.Console/ClassWrite.cs
+++ ClassServer/ClassServer.Console/ClassWrite.cs
@@ -2,9 +2,26 @@ namespace ClassServer.Console;
 
 class ClassWrite : Any
 {
+    public override bool Init()
+    {
+        base.Init();
+        this.CountOperate = new ClassWriteCountOperate();
+        this.CountOperate.Write = this;
+        this.CountOperate.Init();
+
+        this.SetOperate = new ClassWriteSetOperate();
+        this.SetOperate.Write = this;
+        this.SetOperate.Init();
+        return true;
+    }
+
+
     public virtual int Start { get; set; }
     public virtual Data Data { get; set; }
     public virtual ClassWriteArg Arg { get; set; }
+    protected virtual ClassWriteOperate Operate { get; set; }
+    protected virtual ClassWriteCountOperate CountOperate { get; set; }
+    protected virtual ClassWriteSetOperate SetOperate { get; set; }
 
     public virtual bool Execute()
     {
@@ -13,12 +30,38 @@ class ClassWrite : Any
         arg.Init();
         this.Arg = arg;
 
-        
+        this.Operate = this.CountOperate;
+        this.ResetStageIndex();
+        this.ExecuteStage();
 
+        int count;
+        count = this.Start + arg.Index;
+
+        Data data;
+        data = new Data();
+        data.Count = count;
+        data.Init();
+
+        arg.Data = data;
+
+        this.Operate = this.SetOperate;
+        this.ResetStageIndex();
+        this.ExecuteStage();
 
         this.Data = arg.Data;
 
         this.Arg = null;
+        return true;
+    }
+
+    protected virtual bool ResetStageIndex()
+    {
+        this.Arg.Index = 0;
+        return true;
+    }
+
+    protected virtual bool ExecuteStage()
+    {
         return true;
     }
 }
