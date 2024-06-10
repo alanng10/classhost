d8565011da4f6d8767f003217d0047a090993322
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-06-10 20:55:36 +0800

    Script

--- Script/ClassServer/CleanModule.cmd
+++ Script/ClassServer/CleanModule.cmd
@@ -3,5 +3,4 @@
 echo Clean Module
 set DotNetModuleOutFold=.\Out\net8.0
 del /F /Q %DotNetModuleOutFold%\ClassServer.* 2>NUL
-del /F /Q %DotNetModuleOutFold%\ClassServerExe* 2>NUL
 rmdir /S /Q %DotNetModuleOutFold%\ClassServer.Console.data 2>NUL
--- Script/ClassServer/Make.cmd
+++ Script/ClassServer/Make.cmd
@@ -1,5 +1,5 @@
 @echo off
 
-call Script\Class\CleanModule
+call Script\ClassServer\CleanModule
 echo:
-call Script\Class\MakeModule
+call Script\ClassServer\MakeModule
