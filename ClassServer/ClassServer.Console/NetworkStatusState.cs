d77d3511a00f6cfd104c2addfb6b3e88f6650318
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-06-10 20:38:26 +0800

    ClassServer

---
+++ ClassServer/ClassServer.Console/NetworkStatusState.cs
@@ -0,0 +1,48 @@
+namespace ClassServer.Console;
+
+class NetworkStatusState : State
+{
+    public override bool Init()
+    {
+        base.Init();
+        this.NetworkStatusList = NetworkStatusList.This;
+        return true;
+    }
+
+    public virtual Console Console { get; set; }
+    protected virtual NetworkStatusList NetworkStatusList { get; set; }
+
+    public override bool Execute()
+    {
+        NetworkStatusList statusList;
+        statusList = this.NetworkStatusList;
+
+        Console console;
+        console = this.Console;
+
+        Network network;
+        network = console.Network;
+
+        int stage;
+        stage = console.Stage;
+
+        NetworkStatus status;
+        status = network.Status;
+
+        if (stage == 0)
+        {
+
+        }
+
+        if (stage == 1)
+        {
+            if (!(status == statusList.NoError))
+            {
+                network.Close();
+                console.Status = 100 + status.Index;
+            }
+        }
+
+        return true;
+    }
+}
