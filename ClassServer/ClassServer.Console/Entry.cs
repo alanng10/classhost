759bfd1f2955f0f6ead2d6d8e6aa45c251079b44
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-06-11 11:00:32 +0800

    ClassServer

--- ClassServer/ClassServer.Console/Entry.cs
+++ ClassServer/ClassServer.Console/Entry.cs
@@ -4,18 +4,46 @@ public class Entry : EntryEntry
 {
     protected override int ExecuteMain()
     {
+        TextInfra textInfra;
+        textInfra = TextInfra.This;
+
+        Array arg;
+        arg = this.Arg;
+
+        if (arg.Count < 2)
+        {
+            return 10;
+        }
+
         string hostName;
-        hostName = (string)this.Arg.Get(0);
+        hostName = (string)arg.Get(0);
+
+        IntParse parse;
+        parse = new IntParse();
+        parse.Init();
+
+        string ka;
+        ka = (string)arg.Get(1);
+
+        Text k;
+        k = textInfra.TextCreateStringData(ka, null);
+
+        long nn;
+        nn = parse.Execute(k, 10, false);
+
+        int serverPort;
+        serverPort = (int)nn;
 
         Console a;
         a = new Console();
         a.Init();
 
         a.HostName = hostName;
+        a.ServerPort = serverPort;
         a.Execute();
 
         int o;
-        o = a.Status;
+        o = a.Status + 100;
         return o;
     }
 }
