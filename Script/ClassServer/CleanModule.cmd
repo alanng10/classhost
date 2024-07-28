083fcf3ddcbb6996fe4f4974a154e349da25eb31
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-07-28 23:30:08 +0800

    Script

--- Script/ClassServer/CleanModule.cmd
+++ Script/ClassServer/CleanModule.cmd
@@ -1,6 +1,6 @@
 @echo off
 
 echo Clean Module
-set DotNetModuleOutFold=.\Out\net8.0
+set DotNetModuleOutFold=..\Class\Out\net8.0
 del /F /Q %DotNetModuleOutFold%\ClassServer.* 2>NUL
 rmdir /S /Q %DotNetModuleOutFold%\ClassServer.Console.data 2>NUL
--- Script/ClassServer/MakeModule.cmd
+++ Script/ClassServer/MakeModule.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 echo Make Module
-set DotNetModuleOutFold=.\Out\net8.0
+set DotNetModuleOutFold=..\Class\Out\net8.0
 pushd ClassServer\ClassServerExe
 dotnet build -v quiet
 popd
