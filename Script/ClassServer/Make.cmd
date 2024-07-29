06c03c40ab63f6c0364a475f7a2a58ec722ec8c2
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-07-29 16:22:43 +0800

    Script

--- Script/ClassServer/Make.cmd
+++ Script/ClassServer/Make.cmd
@@ -1,23 +1,5 @@
 @echo off
 
-pushd ..\Class
-call Script\Class\CleanModule
-popd
-echo:
-
 call Script\ClassServer\CleanModule
 echo:
-
-pushd ..\Class
-call Script\Tool\CleanBinary
-popd
-echo:
-
 call Script\ClassServer\MakeModule
-echo:
-
-pushd ..\Class
-echo Make Binary
-call Script\Tool\Make ReferBinaryGen
-call Script\Tool\Execute ReferBinaryGen
-popd
