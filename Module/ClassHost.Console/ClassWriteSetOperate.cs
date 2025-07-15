675e49ed5a4f4bc06c9be9d4e7e667b5263cc60d
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-07-15 11:04:12 +0800

    Module

--- Module/ClassHost.Console/ClassWriteSetOperate.cs
+++ Module/ClassHost.Console/ClassWriteSetOperate.cs
@@ -4,25 +4,13 @@ class ClassWriteSetOperate : ClassWriteOperate
 {
     public override bool ExecuteByte(long value)
     {
-        ClassWrite write;
-        write = this.Write;
-
         ClassWriteArg arg;
-        arg = write.Arg;
+        arg = this.Write.Arg;
 
         long index;
         index = arg.Index;
 
-        Data data;
-        data = arg.Data;
-
-        long start;
-        start = write.Start;
-
-        long k;
-        k = start + index;
-
-        data.Set(k, value);
+        arg.Data.Set(index, value);
 
         arg.Index = index + 1;
         return true;
