d0f66f1864bf0120dc6a3d6e729fa1e55dd8297e
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-07-15 12:16:26 +0800

    Module

--- Module/ClassHost.Console/ClassWriteSetOperate.cs
+++ Module/ClassHost.Console/ClassWriteSetOperate.cs
@@ -2,6 +2,15 @@ namespace ClassHost.Console;
 
 class ClassWriteSetOperate : ClassWriteOperate
 {
+    public override bool Init()
+    {
+        base.Init();
+        this.InfraInfra = InfraInfra.This;
+        return true;
+    }
+
+    protected virtual InfraInfra InfraInfra { get; set; }
+
     public override bool ExecuteByte(long value)
     {
         ClassWriteArg arg;
@@ -13,6 +22,20 @@ class ClassWriteSetOperate : ClassWriteOperate
         arg.Data.Set(index, value);
 
         arg.Index = index + 1;
+        return true;
+    }
+
+    public override bool ExecuteInt(long value)
+    {
+        ClassWriteArg arg;
+        arg = this.Write.Arg;
+
+        long index;
+        index = arg.Index;
+
+        this.InfraInfra.DataIntSet(arg.Data, index, value);
+
+        arg.Index = index + sizeof(long);
         return true;
     }
 
